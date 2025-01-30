6eb314fe5f8c57043d906336ff95248a4ee8d295
ROGERIO F MACHADO <162634905+eldorado1959@users.noreply.github.com>
2025-01-30 20:59:27 -0300

    Add files via upload

---
+++ AdicionarComArrayList.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Botao;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Aluno
+ */
+public class AdicionarComArrayList {
+    public static void main(String[]args){
+        ArrayList<Integer> numeros = new ArrayList<>();  // criar um arraylist
+    }
+}
